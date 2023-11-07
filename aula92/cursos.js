1607539dda6e2bfec9b6672aae373b8ec4483b19
JoaoPedroRibeiroLima <111246249+JoaoPedroRibeiroLima@users.noreply.github.com>
2023-11-07 15:54:12 -0300

    aula92

---
+++ aula92/cursos.js
@@ -0,0 +1,4 @@
+const cursos = ["Javascript", "HTML", "CSS", "Arduino", "Raspberry", "C++", "Python", "Java", "C#"]
+const carros = ["Polo", "T-Cross", "Golf", "Onix", "Cruze", "Argo", "Cronnos"]
+
+export {cursos, carros}
---
+++ aula92/script.js
@@ -0,0 +1,7 @@
+import { cursos } from "./cursos.js";
+import { carros } from "./cursos.js";
+
+console.log("server : rodando")
+
+console.log(cursos)
+console.log(carros)
