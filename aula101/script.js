24c89b2bd91258f18aaaf16668549e91661525a3
JoaoPedroRibeiroLima <111246249+JoaoPedroRibeiroLima@users.noreply.github.com>
2023-11-15 23:15:44 -0300

    aula101

---
+++ aula101/script.js
@@ -0,0 +1,10 @@
+const nome = 'JoãoPedro'
+const canal = ' CFBCursos'
+
+console.log(nome.charAt())
+
+console.log(nome.concat(canal))
+
+console.log(nome.indexOf("o"))
+
+console.log(nome.lastIndexOf("o"))
