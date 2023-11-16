87d0feca4c773e3280ca09e0c4068dc3924d5c0d
JoaoPedroRibeiroLima <111246249+JoaoPedroRibeiroLima@users.noreply.github.com>
2023-11-16 00:22:02 -0300

    aula102

---
+++ aula102/script.js
@@ -0,0 +1,11 @@
+const nomeFull = "João Pedro da Silva Ribeiro de Lima"
+
+console.log(nomeFull)
+
+console.log(nomeFull.replace("Pedro", ""))
+
+console.log(nomeFull.slice(6, 10))
+
+console.log(nomeFull.search('Silva'))
+
+console.log(nomeFull.split(" "))
