8a7976adc481aee5419dd616dd8e63d6cabee5d9
JoaoPedroRibeiroLima <111246249+JoaoPedroRibeiroLima@users.noreply.github.com>
2023-11-20 01:17:10 -0300

    aula105

---
+++ aula105/script.js
@@ -0,0 +1,9 @@
+const nome = "João Pedro da Silva Ribeiro de Lima"
+
+console.log(nome)
+
+console.log(nome.search(/p/i))
+
+console.log(nome.match(/o/g))
+
+console.log(nome.replace(/O/ig, 'Teste'))
